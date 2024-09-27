6bfc4e96e0288598e9bcf1fb1ddcc836ca0c548b
ekamva <54266855+ekamva@users.noreply.github.com>
2024-09-27 13:20:58 +0300

    HW2.3.1.1.js - channged to secondGroup and score to Eve

--- HW2.3.1.1.js
+++ HW2.3.1.1.js
@@ -5,7 +5,7 @@ const secondGroup = [
     { id: 2, name: "Bob", scorePoints: 6200 },
     { id: 3, name: "Charlie", scorePoints: 4800 },
     { id: 4, name: "Diana", scorePoints: 5900 },
-    { id: 5, name: "Eve", scorePoints: 6200 }
+    { id: 5, name: "Eve", scorePoints: 6700 }
 ];
 
 for (let elementKey in secondGroup) {
@@ -22,4 +22,4 @@ for (let elementKey in players){
 }
 
 
-console.log(`Player ${bestPlayer} has the best score of ${bestScore}!`)    
+console.log(`secondGroup ${bestPlayer} has the best score of ${bestScore}!`)    
